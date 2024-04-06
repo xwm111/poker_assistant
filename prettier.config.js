77469c75fc9408ae55dab99dc32994841db9768b
weiming <swimming.xwm@gmail.com>
2024-04-06 19:56:33 +0800

    配置监听地址为 0.0.0.0

--- prettier.config.js
+++ prettier.config.js
@@ -3,4 +3,8 @@ const styleguide = require('@vercel/style-guide/prettier');
 module.exports = {
   ...styleguide,
   plugins: [...styleguide.plugins, 'prettier-plugin-tailwindcss'],
+  serverRuntimeConfig: {
+    // 监听地址
+    host: '0.0.0.0',
+  },
 };
